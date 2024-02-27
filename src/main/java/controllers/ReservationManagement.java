1e6d1fbdb0a80610ef0f0afd1f4d026ca8724850
cherif191902 <cherif.benhassine@esprit.tn>
2024-02-27 14:30:05 +0100

    ByteCoders-CarthagoSmart

--- src/main/java/controllers/ReservationManagement.java
+++ src/main/java/controllers/ReservationManagement.java
@@ -2,6 +2,7 @@ package controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.SplitMenuButton;
 import javafx.scene.control.TextField;
@@ -36,6 +37,16 @@ public class ReservationManagement {
         int numero= Integer.parseInt(txtnumero.getText());
         int nbr_place = Integer.parseInt(txtnbr_place.getText());
 
+        if (!isValidEmail(email)) {
+            showAlert("Format d'email invalide", "Veuillez saisir un email valide.");
+            return;
+        }
+
+
+
+
+
+
         // Créer un objet Reservation avec les données récupérées
         Reservation reservation = new Reservation(nom_event, email,numero, nbr_place);
 
@@ -50,6 +61,24 @@ public class ReservationManagement {
             // Gérer l'erreur, par exemple, afficher un message à l'utilisateur indiquant que la réservation a échoué.
         }
     }
+
+
+
+    private boolean isValidEmail(String email) {
+        // Expression régulière pour valider le format d'email
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+        return email.matches(emailRegex);
+    }
+
+
+    private void showAlert(String message, String title) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
     private void clearFields() {
         txtnom_event.clear();
         txtemail.clear();
